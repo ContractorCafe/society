2c152811e62d4bda27a7d2431cceffa02a6c2508
DR <vivaladan@gmail.com>
2017-09-10 12:25:05 +0100

    Added placeholder test project

---
+++ society.contractor.cafe.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace society.contractor.cafe.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
