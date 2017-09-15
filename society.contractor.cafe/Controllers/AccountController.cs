64592d5ee48f7bfcd73ee8c6a9e64eae7d6020c6
Dan Revell <vivaladan@gmail.com>
2017-09-15 22:09:38 +0100

    Login working against Auth0

---
+++ society.contractor.cafe/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿// Controllers/AccountController.cs
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class AccountController : Controller
+{
+    public async Task Login(string returnUrl = "/")
+    {
+        await HttpContext.ChallengeAsync("Auth0", new AuthenticationProperties() { RedirectUri = returnUrl });
+    }
+
+    [Authorize]
+    public async Task Logout()
+    {
+        await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+        {
+            RedirectUri = Url.Action("Index", "Home")
+        });
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
--- society.contractor.cafe/Startup.cs
+++ society.contractor.cafe/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
 
 namespace society.contractor.cafe
 {
@@ -20,35 +21,13 @@ namespace society.contractor.cafe
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(
-                options => options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+            Authentication.Auth0.Setup(services, Configuration);
 
             services.AddMvc();
-
-            services.AddOptions();
-
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                })
-            .AddCookie()
-            .AddOpenIdConnect(options =>
-                {
-                    options.Authority = $"https://{Configuration["Auth0:Domain"]}";
-                    options.ClientId = Configuration["Auth0:ClientId"];
-                    options.ClientSecret = Configuration["Auth0:ClientSecret"];
-                    options.ResponseType = "code";
-                    options.CallbackPath = new Microsoft.AspNetCore.Http.PathString("/signin-auth0");
-                    options.ClaimsIssuer = "Auth0";
-
-                });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -61,9 +40,9 @@ namespace society.contractor.cafe
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseAuthentication();
-
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
